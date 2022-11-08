e5a563d20f5d016ad3295fbffcbb8866371966bf
John Alexander Acevedo Serna <johnalexanderacevedo@gmail.com>
2022-11-08 10:37:29 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #return HttpResponse(' <h1>Bienvenido puto a inicio</h1>')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'John Ace'})
